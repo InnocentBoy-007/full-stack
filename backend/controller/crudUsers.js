f00849a11451d2c421b40f043f0b01b014d83de4
unknown <mechanshil7@gmail.com>
2024-10-26 21:00:51 +0530

    Added a new file that controls users

--- backend/controller/crudUsers.js
+++ backend/controller/crudUsers.js
@@ -18,3 +18,25 @@ export const fetchUsers = async(req, res) => {
         })
     }
 }
+
+// export const pathUsers = async(req, res) => {
+//     const {id} = req.params;
+//     const {updates} = req.body;
+//     try {
+//         const user = await User.findById(id);
+//         if(!user) return res.status(404).json({
+//             message:"User not found! - backend"
+//         })
+//         Object.assign({user, updates});
+//         const patchUsers = await user.save();
+//         res.status(200).json({
+//             message:"Profile updated successfully! - backend",
+//             patchUsers
+//         })
+//     } catch (error) {
+//         res.status(500).json({
+//             message:"Internal server error! - backend",
+//             error
+//         })
+//     }
+// }
